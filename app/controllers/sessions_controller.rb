69813317dca04fbd654727db73d9467c493b1223
Peter Wood <email": null>
2016-01-22 15:37:37 -0500

    Add new and create routes for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user and @user.password == params[:user][:password]
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def delete

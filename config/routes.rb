e9457fde946dfa29428d00f8ab308322e9e4bebb
Peter Wood <email": null>
2016-01-22 16:34:03 -0500

    Add users#create and #new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'questions#index'
   resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

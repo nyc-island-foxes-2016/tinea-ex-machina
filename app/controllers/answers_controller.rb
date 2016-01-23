eb8e3fc11abb3e2774894379e896ded18a843586
Darius Atmar <dariusatmar@Dariuss-MacBook-Pro-2.local>
2016-01-23 16:58:11 -0500

    Add answers#create and private params for Qs and As

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,12 @@
 class AnswersController < ApplicationController
+	def create
+		@answer = Answer.create(answer_params)
+		binding.pry
+	end
 
+  private 
+
+  def answer_params
+  	params.require(:answer).permit(:content, :user_id)
+  end
 end

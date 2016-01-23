7cc6a88c46be995f0a93e70b79aa24a5a887adfb
Peter Wood <email": null>
2016-01-23 12:07:21 -0500

    Update sessions#new to handle xhr requests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
   def new
     @user = User.new
+    if request.xhr?
+      render '_form', layout: false, locals: {user: @user}
+    end
   end
 
   def create

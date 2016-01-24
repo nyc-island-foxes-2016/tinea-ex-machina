9bfcf1f9bdf5de9d5dcf82ee692eae5f992254dc
caromedellin <caromedellin@gmail.com>
2016-01-24 17:54:31 -0500

    we are able to make a post but we are not getting the proper response, so I'm pushing this for Darius to be able to clone only the branch, do not merge!

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -32,4 +32,25 @@ $(document).ready(function() {
       console.log('Bad: ' + response);
     });
   });
+
+  $('.vote-question').on('submit', '.new_vote', function(submit){
+    submit.preventDefault();
+    var request_url = $(this).attr("action");
+    console.log(request_url);
+    $.ajax({
+      type: "POST",
+      url: request_url,
+      data: $(this).serialize(),
+      dataType: 'json'
+    }).done(function(response){
+      console.log(response);
+    }).fail(function(response){
+      console.log(response);
+      console.log("You suck!");
+    });
+  });
+
+
+
+
 });
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,34 +2,47 @@ class VotesController < ApplicationController
 	def create
 		@votable = find_votable
 			#### TODO write logic for downvote as well
-		if Vote.where(user: current_user, votable_id: @votable.id).empty?
-			@vote = @votable.votes.build(user: current_user)
-			@vote.update_attributes(is_up_vote: true)
-		else
-			@vote = Vote.new
-		end
-		@question = Question.find_by(id: params[:question_id])
-		if @vote.save
-			redirect_to @question
-		else
-			redirect_to @question
-		end
-	end
+			if Vote.where(user: current_user, votable_id: @votable.id).empty?
+				@vote = @votable.votes.build(user: current_user)
+				@vote.update_attributes(is_up_vote: true)
+			else
+				@vote = Vote.new
+			end
+			@question = Question.find_by(id: params[:question_id])
+			if @vote.save
 
-	private
+				if request.xhr?
+					binding.pry
+					puts @votable.votes.count.to_json
+					return @votable.votes.count.to_json
 
-	def vote_params
-		vote_params = params.require(:vote).permit(:user_id)
 
-	end
 
-	def find_votable
-		params.each do |name, value|
-			if name =~ /(.+)_id$/
-				return $1.classify.constantize.find(value)
+
+
+				else
+					puts ("Nojson")
+					redirect_to @question
+				end
+			else
+				redirect_to @question
 			end
 		end
-		nil
-	end
 
-end
+		private
+
+		def vote_params
+			vote_params = params.require(:vote).permit(:user_id)
+
+		end
+
+		def find_votable
+			params.each do |name, value|
+				if name =~ /(.+)_id$/
+					return $1.classify.constantize.find(value)
+				end
+			end
+			nil
+		end
+
+	end

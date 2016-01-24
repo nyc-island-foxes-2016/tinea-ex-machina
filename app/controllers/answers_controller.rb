c634f681326cb7da0014de356c05258c73a70a9d
Peter Wood <email": null>
2016-01-24 16:34:02 -0500

    Remove answer#update from controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,16 +5,9 @@ class AnswersController < ApplicationController
 		redirect_to question_path(params[:question_id])
 	end
 
-  def update
-    @answer = Answer.find(params[:id])
-    @answer.update_attribute(:best, true)
-    redirect_to @answer.question
-  end
-
   private 
 
   def answer_params
   	params.require(:answer).permit(:content, :user_id)
-
   end
 end

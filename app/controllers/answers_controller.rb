7655377f53d264d6d63dc3fca8a869b67abce212
Darius Atmar <dariusatmar@Dariuss-MacBook-Pro-2.local>
2016-01-24 13:51:26 -0500

    Fix current_user method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,6 +2,7 @@ class AnswersController < ApplicationController
 	def create
 		@answer = Answer.new(answer_params)
 		@answer.update_attributes(user: current_user, question_id: params[:question_id])
+		binding.pry
 		redirect_to question_path(params[:question_id])
 	end
 

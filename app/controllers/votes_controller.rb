a950bfd528ff0fcd4f6ec2345a61e29ff681fd45
Darius Atmar <dariusatmar@Dariuss-MacBook-Pro-2.local>
2016-01-24 14:50:16 -0500

    Add preliminary votes#create route in votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,1 +1,26 @@
-votes_controller.rb
+class VotesController < ApplicationController
+	def create
+		@votable = find_votable
+		@vote = @votable.votes.build(user: current_user) #Might 
+		#### TODO write logic for downvote as well
+			@vote.update_attributes(is_up_vote: true)
+		binding.pry
+	end
+
+	private
+
+	def vote_params
+		vote_params = params.require(:vote).permit(:user_id)
+
+	end
+
+	def find_votable
+		params.each do |name, value|
+			if name =~ /(.+)_id$/
+				return $1.classify.constantize.find(value)
+			end
+		end
+		nil
+	end
+
+end
